af3aecaaedb4ef8979f564a55041b48a03b9fcb3
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-07-21 01:52:46 +0800

    fix: use ECS v0.1.150 hard-deadline release

--- internal/appmeta/version.go
+++ internal/appmeta/version.go
@@ -3,8 +3,8 @@ package appmeta
 var (
 	AppID              = "com.oneclickvirt.goecs"
 	AppName            = "goecs"
-	Version            = "0.1.149"
-	UpstreamECSVersion = "v0.1.149"
+	Version            = "0.1.150"
+	UpstreamECSVersion = "v0.1.150"
 )
 
 func ReleaseVersion() string {
