aebdf80e031ada0c70a38601405e64ec32b65e0c
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-07-21 13:40:02 +0800

    release: link ecs v0.1.155

--- internal/appmeta/version.go
+++ internal/appmeta/version.go
@@ -3,8 +3,8 @@ package appmeta
 var (
 	AppID              = "com.oneclickvirt.goecs"
 	AppName            = "goecs"
-	Version            = "0.1.155"
-	UpstreamECSVersion = "v0.1.154"
+	Version            = "0.1.156"
+	UpstreamECSVersion = "v0.1.155"
 )
 
 func ReleaseVersion() string {
