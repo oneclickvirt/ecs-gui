0f14c372a3c76ad29b12f79ab2c53056a096d7c0
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-07-21 18:01:11 +0800

    release: automate tagged GUI builds

--- internal/appmeta/version.go
+++ internal/appmeta/version.go
@@ -3,8 +3,8 @@ package appmeta
 var (
 	AppID              = "com.oneclickvirt.goecs"
 	AppName            = "goecs"
-	Version            = "0.1.157"
-	UpstreamECSVersion = "v0.1.156"
+	Version            = "0.1.158"
+	UpstreamECSVersion = "v0.1.157"
 )
 
 func ReleaseVersion() string {
