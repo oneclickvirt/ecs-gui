fa07784edbd504b814073757c4272a832d84df38
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-07-22 15:10:23 +0800

    fix: read ecs version from public API metadata

--- internal/appmeta/version.go
+++ internal/appmeta/version.go
@@ -3,7 +3,7 @@ package appmeta
 var (
 	AppID              = "com.oneclickvirt.goecs"
 	AppName            = "goecs"
-	Version            = "0.1.168"
+	Version            = "0.1.169"
 	UpstreamECSVersion = "v0.1.165"
 )
 
