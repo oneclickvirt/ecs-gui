fe9e813d9009ee5fcddda71011cc04358da92f13
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-07-22 10:42:48 +0800

    feat: align GUI with goecs v0.1.161

--- internal/appmeta/version.go
+++ internal/appmeta/version.go
@@ -3,8 +3,8 @@ package appmeta
 var (
 	AppID              = "com.oneclickvirt.goecs"
 	AppName            = "goecs"
-	Version            = "0.1.162"
-	UpstreamECSVersion = "v0.1.160"
+	Version            = "0.1.163"
+	UpstreamECSVersion = "v0.1.161"
 )
 
 func ReleaseVersion() string {
