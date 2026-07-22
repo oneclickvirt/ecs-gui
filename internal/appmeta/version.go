36a01179cf9e0c4367aaeb192227791f4e81e54b
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-07-23 03:22:01 +0800

    chore: consume goecs v0.1.170

--- internal/appmeta/version.go
+++ internal/appmeta/version.go
@@ -3,8 +3,8 @@ package appmeta
 var (
 	AppID              = "com.oneclickvirt.goecs"
 	AppName            = "goecs"
-	Version            = "0.1.172"
-	UpstreamECSVersion = "v0.1.169"
+	Version            = "0.1.173"
+	UpstreamECSVersion = "v0.1.170"
 )
 
 func ReleaseVersion() string {
