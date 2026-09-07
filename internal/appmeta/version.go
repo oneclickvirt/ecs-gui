e0d52baae2610193fe5f5eb298d7eb87a189064a
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2026-09-07 15:18:43 +0800

    ci: align GUI release dependency contract

--- internal/appmeta/version.go
+++ internal/appmeta/version.go
@@ -3,7 +3,7 @@ package appmeta
 var (
 	AppID              = "com.oneclickvirt.goecs"
 	AppName            = "goecs"
-	Version            = "0.2.1"
+	Version            = "0.2.2"
 	UpstreamECSVersion = "v0.2.1"
 )
 
